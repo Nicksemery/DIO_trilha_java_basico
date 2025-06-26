62ce998c3c96346b23b309156a4fe4b1b8a3f508
Nicolas <nicolasrodriguesemery@gmail.com>
2025-06-26 18:47:21 -0300

    meu projeto conta banco

---
+++ ContaBanco/src/Main.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Main {
+
+    private final static String BemVindo = "Olá e Bem Vindo!";
+
+    public static void main(String[] args) {
+        var sc = new Scanner(System.in);
+
+        System.out.println(BemVindo);
+        System.out.println("Por favor, informe seu nome completo!");
+        var nome = sc.nextLine();
+
+        System.out.println("Por favor, informe o numero da agencia");
+        var agencia = sc.nextLine();
+
+        System.out.println("Por favor, informe o numero da conta");
+        var numero = sc.nextInt();
+
+        System.out.println("Por favor, informe seu Salário");
+        var Saldo = sc.nextDouble();
+
+        System.out.printf("Olá %s, obrigado por criar uma conta em nosso banco, sua agência é %s, conta %s e seu saldo %s já está disponível para saque", nome, agencia, numero, Saldo);
+
+    }
+}
